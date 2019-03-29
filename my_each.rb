e779cb96d69ab84cf8020d5611a10cc893b2dab5
Kat Maldon <katharinamaldon@gmail.com>
2019-03-29 15:15:36 +0000

    Automatically backed up by Learn

---
+++ my_each.rb
@@ -0,0 +1,14 @@
+def my_each(array)
+  if block_given?
+  n = 0 
+    while n < array.length 
+      yield(array[n])
+      n += 1 
+    end
+  
+    array
+  
+  else 
+    puts "No array given."
+  end
+end 
